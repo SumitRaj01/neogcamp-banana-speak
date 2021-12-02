90e24b81d516af4d9ae646b40301a58b495ffbee
SumitRaj01 <Sumeet23051994@gmail.com>
2021-12-02 20:29:19 +0530

    js file added

---
+++ app.js
@@ -0,0 +1,10 @@
+//console.log("script is working from a different file");
+//input
+
+var username = prompt("Give me your username");
+
+//processing
+var welcomeMessage = "This script works! " + username;
+
+//output
+alert(welcomeMessage);
